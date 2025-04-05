ccfd3d324d2e07d16b824451cd81d432b332cbf2
Artem Osta <ostart@mail.ru>
2025-04-05 07:15:01 +0300

    Refactor to escape runtime exception

--- TacacsPlusCore/Authentication/MsChapV1.cs
+++ TacacsPlusCore/Authentication/MsChapV1.cs
@@ -33,10 +33,11 @@ namespace Petrsnd.TacacsPlusCore.Authentication
             var challenge = new byte[8];
             Rng.GetBytes(challenge, 0, 8);
 
-            // RFC 2433 6 states that the LAN MAnager compatible challenge has been deprecated
+            // RFC 2433 6 states that the LAN Manager compatible challenge has been deprecated
             // and should not be sent.  Instead this should be zero-filled.
-            var lmChallengeResponse = GetLmChallengeResponse(challenge, password);
-            Buffer.BlockCopy(Enumerable.Repeat((byte)0x00, 24).ToArray(), 0, lmChallengeResponse, 0, 24);
+            // var lmChallengeResponse = GetLmChallengeResponse(challenge, password);
+            // Buffer.BlockCopy(Enumerable.Repeat((byte)0x00, 24).ToArray(), 0, lmChallengeResponse, 0, 24);
+            var lmChallengeResponse = GetLmChallengeResponse();
             // RFC 2433 6, also see A.5
             var ntChallengeResponse = GetNtChallengeResponse(challenge, password);
 
@@ -68,33 +69,39 @@ namespace Petrsnd.TacacsPlusCore.Authentication
 
             return authenticationData;
         }
-
-        public static byte[] GetLmChallengeResponse(byte[] challenge, SecureString password)
+        
+        public static byte[] GetLmChallengeResponse()
         {
-            var lmPasswordHash = GetLmPasswordHash(password);
-            var key = new byte[21];
-            Buffer.BlockCopy(lmPasswordHash, 0, key, 0, 16);
-            Buffer.BlockCopy(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00 }, 0, key, 16, 5);
-            var input = new byte[24];
-            for (var i = 0; i < 3; i++)
-                Buffer.BlockCopy(challenge, 0, input, i * 8, 8);
-            return DesHash.ComputeHash(key, input);
+            // Note that use of the LAN Manager compatible challenge response has been deprecated; peers SHOULD NOT generate it, and the sub-field SHOULD be zero-filled. See RFC 2433 6.Response Packet
+            return Enumerable.Repeat((byte)0x00, 24).ToArray();
         }
 
-        public static byte[] GetLmPasswordHash(SecureString password)
-        {
-            var passwordBuf = GetLmPasswordBuffer(password);
-            var input = Encoding.ASCII.GetBytes("KGS!@#$%KGS!@#$%");
-            return DesHash.ComputeHash(passwordBuf, input);
-        }
+        // public static byte[] GetLmChallengeResponse(byte[] challenge, SecureString password)
+        // {
+        //     var lmPasswordHash = GetLmPasswordHash(password);
+        //     var key = new byte[21];
+        //     Buffer.BlockCopy(lmPasswordHash, 0, key, 0, 16);
+        //     Buffer.BlockCopy(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00 }, 0, key, 16, 5);
+        //     var input = new byte[24];
+        //     for (var i = 0; i < 3; i++)
+        //         Buffer.BlockCopy(challenge, 0, input, i * 8, 8);
+        //     return DesHash.ComputeHash(key, input);
+        // }
 
-        public static byte[] GetLmPasswordBuffer(SecureString password)
-        {
-            var passwordArray = Encoding.ASCII.GetBytes(password.ToInsecureString().ToUpper()).ToList();
-            for (var i = passwordArray.Count; i < 14; i++)
-                passwordArray.Add(0x00);
-            return passwordArray.Take(14).ToArray();
-        }
+        // public static byte[] GetLmPasswordHash(SecureString password)
+        // {
+        //     var passwordBuf = GetLmPasswordBuffer(password);
+        //     var input = Encoding.ASCII.GetBytes("KGS!@#$%KGS!@#$%");
+        //     return DesHash.ComputeHash(passwordBuf, input);
+        // }
+
+        // public static byte[] GetLmPasswordBuffer(SecureString password)
+        // {
+        //     var passwordArray = Encoding.ASCII.GetBytes(password.ToInsecureString().ToUpper()).ToList();
+        //     for (var i = passwordArray.Count; i < 14; i++)
+        //         passwordArray.Add(0x00);
+        //     return passwordArray.Take(14).ToArray();
+        // }
 
         public static byte[] GetNtChallengeResponse(byte[] challenge, SecureString password)
         {
